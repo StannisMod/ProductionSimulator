d8b4f145de41d98b52db30032633ece3b72422e4
PDDMain <37112091+PDDMain@users.noreply.github.com>
2018-06-07 12:22:29 +0300

    Update conflict

--- app/src/main/java/stannis/ru/productionsimulator/Models/Staff.kt
+++ app/src/main/java/stannis/ru/productionsimulator/Models/Staff.kt
@@ -1,3 +1,9 @@
 package stannis.ru.productionsimulator.Models
 
-class Staff(var name: String, var age:Int, var prof: String, var quality:Int, var nation:String, var salary:Int )
+class Staff(var name: String, var age:Int, var prof: String, var quality:Int, var nation:String, var salary:Int ){
+    override fun toString(): String {
+        return "${name}, ${prof}, ${age}, "
+    }
+
+
+}
---
+++ app/src/main/java/stannis/ru/productionsimulator/WorkerActivity.kt
@@ -0,0 +1,12 @@
+package stannis.ru.productionsimulator
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class WorkerActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_worker)
+    }
+}
