7bf26594aae3e86b67b4671a02779831e3ddbf1d
PDDMain <37112091+PDDMain@users.noreply.github.com>
2018-06-07 12:13:27 +0300

    Add backButton

--- app/src/main/java/stannis/ru/productionsimulator/MailActivity.kt
+++ app/src/main/java/stannis/ru/productionsimulator/MailActivity.kt
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
 import android.widget.ListView
+import kotlinx.android.synthetic.main.activity_message.view.*
 
 class MailActivity : AppCompatActivity() {
 
