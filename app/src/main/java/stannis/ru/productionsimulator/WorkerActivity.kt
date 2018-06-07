6225312f300976d2955805e4412ab885bb7c998b
PDDMain <37112091+PDDMain@users.noreply.github.com>
2018-06-07 14:01:17 +0300

    Add mail.setOnClick.... to WorkerActivity

--- app/src/main/java/stannis/ru/productionsimulator/WorkerActivity.kt
+++ app/src/main/java/stannis/ru/productionsimulator/WorkerActivity.kt
@@ -5,12 +5,19 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import kotlinx.android.synthetic.main.activity_worker.*
+import kotlinx.android.synthetic.main.stats_panel.*
 
 class WorkerActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_worker)
+
+        mail.setOnClickListener {
+            val intent = Intent(this, MailActivity::class.java)
+            startActivity(intent)
+        }
+
         if (intent.hasExtra("TAG")) {
             val arr = intent.getStringExtra("TAG").split(".")
             val dataOfWorker = arr[0].split(",")
