06898d4bdefecac77f161691350535554c39c21f
PDDMain <37112091+PDDMain@users.noreply.github.com>
2018-06-15 13:31:46 +0300

    updated Disagn

--- app/src/main/java/stannis/ru/productionsimulator/ListProductionActivity.kt
+++ app/src/main/java/stannis/ru/productionsimulator/ListProductionActivity.kt
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
 import android.widget.ListView
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_market.*
 import stannis.ru.productionsimulator.Models.fillDb
 
 class ListProductionActivity : AppCompatActivity() {
@@ -15,10 +17,12 @@ class ListProductionActivity : AppCompatActivity() {
 
         var listview : ListView = findViewById(R.id.listView)
         val dataArray = Array(1){i -> "Лесопилка"}
-        val adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, dataArray)
+        val adapter = ArrayAdapter<String>(this, android.R.layout.select_dialog_item, dataArray)
         listview.adapter = adapter
 
         listview.setOnItemClickListener { parent, view, position, id ->
+            var text : TextView = findViewById(R.id.text2)
+            text.text = "Подождите около минуты...\nСпасибо за понимание"
             val intent = Intent(this, MainActivity::class.java)
             fillDb(this)
             startActivity(intent)
